7f3666c4d74b07c7b9ad405c3b0f73f0267c7912
robreijtenbach <33518020+robreijtenbach@users.noreply.github.com>
2025-02-11 19:01:31 +0100

    Solved 5

--- problem_5.py
+++ problem_5.py
@@ -6,23 +6,46 @@ class Solution(object):
         """
         
         n = len(s)
-        ret = ""
 
         palindromes = []
 
+        if n == 0 or n == 1:
+            return s
+        
+        if n == 2:
+            ret = s[0]
+            if s[1] == ret:
+                ret+=s[1]
+            return ret
+
+
         #find palindrome middles
 
-        for i in range(n-2):
+        for i in range(n-1):
             if s[i] == s[i+1]:
                 palindromes.append((i,i+1))
-            elif s[i] == s[i+2]:
+            if i < n-2 and s[i] == s[i+2]:
                 palindromes.append((i,i+2))
         
-        ## TODO grow palindromes and return longest
-
-        return ret
+        print(palindromes)
 
 
+        for i in range(len(palindromes)):
+            l,r = palindromes[i]
+            
+            while l > 0 and r < n-1 and s[l-1] == s[r+1]:
+                l-=1
+                r+=1
+            
+            palindromes[i] = (l,r)
+        print(palindromes)
+        
+        if not palindromes:
+            return s[0]
+
+        ret = max(palindromes, key=lambda x: x[1]-x[0])
+
+        return s[ret[0]:ret[1]+1]
 
 
 import unittest
@@ -33,10 +56,23 @@ class Test(unittest.TestCase):
 
         self.assertIn(Solution().longestPalindrome(input), output)
     
-    def test1(self):
+    def test2(self):
         input = "cbbd"
         output = "bb"
 
         self.assertEqual(Solution().longestPalindrome(input), output)
+
+    def test3(self):
+        input = "abb"
+        output = "bb"
+        
+        self.assertEqual(Solution().longestPalindrome(input), output)
+    
+    def test4(self):
+        input = "aaaa"
+        output = "aaaa"
+        
+        self.assertEqual(Solution().longestPalindrome(input), output)
+
 
 unittest.main()
